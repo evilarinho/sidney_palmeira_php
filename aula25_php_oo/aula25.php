02ff4cd399842c642d60df174ccd2e795897ddaa
edilson@Acer-E5-473 <ed.vilarinho@gmail.com>
2021-01-01 07:34:15 -0300

    new file: aula25_php_oo

---
+++ aula25_php_oo/aula25.php
@@ -0,0 +1,44 @@
+<?php
+    # Nessa aula vamos aprender um pouco mais sobre PHP Orientado a Objetos - PHP OO com Herança
+    /** PHP OO
+     * 
+     * classe Veiculo
+     *      + marca
+     *      + modelo
+     *      + cor
+     * 
+     *      mostrarFicha()
+     *          this->marca
+     *          this->modelo 
+     *          this->cor
+     *          this->placa  
+     * 
+     * clsse Caminhao extends Veiculo
+     *        + pesoLiquido
+     *        + capacidadeDeCarga
+     * 
+     * *      mostrarFichaCaminhao()
+     *          this->marca
+     *          this->modelo 
+     *          this->cor 
+     *          this->peloLiquido          
+     *          this->capacidadeDeCarga
+     * 
+     * * clsse Moto extends Veiculo
+     *        + cilindradas
+     *   
+     *        mostrarFichaMoto()
+     *          this->marca
+     *          this->modelo 
+     *          this->cor 
+     *          this->cilindrada          
+     *          
+     * Instanciar os objetos exibir suas respectivas fichas:
+     * - $veiculo1
+     * - $veiculo2
+     * - $caminhao1
+     * - $moto1
+     */
+
+
+?>
