0e7a0568bc4b3aa728c2755f861c3d9c138b1f30
edilson@Acer-E5-473 <ed.vilarinho@gmail.com>
2021-01-08 06:34:34 -0300

    modified: aula02_variaveis/ modified: aula27_crud_create/

--- aula02_variaveis/aula02.php
+++ aula02_variaveis/aula02.php
@@ -3,7 +3,7 @@
     $nome = "Sidney";
     $SobreNome = " Palmeira de Oliveira";
     $idade = 42;
-
+    //echo var_dump($idade);
     echo "Meu nome completo é: " . $nome . $SobreNome . ' ** ' . $idade; 
 
 ?>     
