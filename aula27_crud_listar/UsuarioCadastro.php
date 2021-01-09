01b9e92e014c6e1c289e890ad5d809c8adef6737
edilson@Acer-E5-473 <ed.vilarinho@gmail.com>
2021-01-09 07:01:39 -0300

    new file: aula27_crud_listar/

---
+++ aula27_crud_listar/UsuarioCadastro.php
@@ -0,0 +1,48 @@
+<!-- 
+    ************************************************************************
+    Nessa aula vamos listar os dados dos usuários, gravados no arquivo texto
+    ************************************************************************
+-->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Cadastro de Usuário em TXT</title>
+</head>
+<body>
+
+<form method="post" action="UsuarioController.php">
+
+    <table align="center" width="300" border="1">
+        <tr>
+            <td colspan="2" bgcolor="lightgray" align="center">
+                Cadastro de Usuário
+            </td>
+        </tr>
+
+        <tr>
+            <td align="right">Nome</td>
+            <td> <input type="text" name="txtNome"> </td>
+        </tr>
+
+        <tr>
+            <td align="right">E-mail</td>
+            <td> <input type="email" name="txtEmail"> </td>
+        </tr>
+
+        <tr>
+            <td align="right">Senha</td>
+            <td> <input type="password" name="txtSenha"> </td>
+        </tr>
+
+        <tr>
+            <td colspan="2" bgcolor="lightgray" align="center">
+                <input type="submit" value="Gravar">
+            </td>
+        </tr>          
+    </table>
+</form>
+    
+</body>
+</html>
